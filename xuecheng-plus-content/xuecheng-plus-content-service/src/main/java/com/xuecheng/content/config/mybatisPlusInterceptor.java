314558a9b14adf77b60822238a8d38f5f25da402
rui <ungatz@hotmail.com>
2023-01-28 22:47:23 +0800

    day2_04接口测试

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/config/mybatisPlusInterceptor.java
@@ -0,0 +1,26 @@
+package com.xuecheng.content.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: xuecheng
+ * @description:
+ * @author: Ethercat
+ * @create: 2023-01-28 20:25
+ **/
+@Configuration
+@MapperScan("com.xuecheng.content.mapper")
+public class mybatisPlusInterceptor {
+
+    @Bean
+    public MybatisPlusInterceptor getmybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+}
